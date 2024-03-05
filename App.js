f24929bec04dad4e59a602015fcc323f20def948
fbansept <bansept.franck@gmail.com>
2024-03-05 14:27:42 +0100

    add safearea provider

--- App.js
+++ App.js
@@ -2,35 +2,45 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import AppButton from "./components/AppButton/AppButton";
 import theme from "./theme";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
+
+  const App = () => {
   
-  const colors = theme();
+    const colors = theme();
 
-  const onPressClicMoi = () => {
-    console.log("Hello !");
-  };
+    const margin = useSafeAreaInsets();
 
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: colors.background,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    monTexte: {
-      color: "blue",
-    },
-  });
+    const onPressClicMoi = () => {
+      console.log("Hello !");
+    };
+
+    const styles = StyleSheet.create({
+      container: {
+        flex: 1,
+        backgroundColor: colors.background,
+        alignItems: "center",
+        justifyContent: "start",
+      },
+      monTexte: {
+        color: "blue",
+      },
+    });
+
+    return (
+      <View style={styles.container}>
+        <AppButton title="Clic moi" onPress={onPressClicMoi}></AppButton>
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 
   return (
-    <View style={styles.container}>
-      <AppButton title="Clic moi" onPress={onPressClicMoi}></AppButton>
-      <StatusBar style="auto" />
-    </View>
-  );
-
-  
+    <SafeAreaProvider>
+      <App></App>
+    </SafeAreaProvider>
+  )
 }
 
 

f11317a0006d2808166e7cc9ce96ee8eef2f081d
fbansept <bansept.franck@gmail.com>
2024-03-05 14:49:36 +0100

    add safearea

--- App.js
+++ App.js
@@ -1,46 +1,28 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import AppButton from "./components/AppButton/AppButton";
-import theme from "./theme";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import AppStyles from "./AppStyles";
 
 export default function App() {
-
-  const App = () => {
-  
-    const colors = theme();
-
-    const margin = useSafeAreaInsets();
+  const EcranPrincipal = () => {
+    const styles = AppStyles();
 
     const onPressClicMoi = () => {
       console.log("Hello !");
     };
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: colors.background,
-        alignItems: "center",
-        justifyContent: "start",
-      },
-      monTexte: {
-        color: "blue",
-      },
-    });
-
     return (
-      <View style={styles.container}>
+      <View style={styles.safeArea}>
         <AppButton title="Clic moi" onPress={onPressClicMoi}></AppButton>
         <StatusBar style="auto" />
       </View>
     );
-  }
+  };
 
   return (
     <SafeAreaProvider>
-      <App></App>
+      <EcranPrincipal></EcranPrincipal>
     </SafeAreaProvider>
-  )
+  );
 }
-
-
---
+++ AppStyles.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+import theme from "./theme";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+export default AppStyles = () => {
+  const colorsTheme = theme();
+  const margins = useSafeAreaInsets();
+
+  return StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: colorsTheme.background,
+      alignItems: "center",
+      justifyContent: "start",
+    },
+    safeArea: {
+      paddingTop: margins.top,
+      paddingBottom: margins.bottom,
+      paddingLeft: margins.left,
+      paddingRight: margins.right,
+    },
+  });
+};
